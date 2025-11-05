1c670da86fd7700a26f0a463556be9608333c565
HarAshish Arora <77282722+thundercrypt@users.noreply.github.com>
2025-11-05 14:43:39 +0530

    Update build.sh to include cowsay Easter Egg

--- build.sh
+++ build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-# Build script for SocialNet Simulator
-# Compiles main.cpp and produces executable 'socialnet'
+
+MSG="COL106 dassi dedo pls"
+
+ANIMALS=(tux dragon stegosaurus vader turkey skeleton moose eyes ghostbusters)
+
+if command -v cowsay >/dev/null 2>&1; then
+    idx=$((RANDOM % ${#ANIMALS[@]}))
+    cowfile=${ANIMALS[$idx]}
+    cowsay -f "$cowfile" "$MSG"
+elif command -v cowthink >/dev/null 2>&1; then
+    idx=$((RANDOM % ${#ANIMALS[@]}))
+    cowfile=${ANIMALS[$idx]}
+    cowthink -f "$cowfile" "$MSG"
+else
+    echo "+-----------------------------------------------+"
+    printf "| %-45s |\n" "$MSG"
+    echo "+-----------------------------------------------+"
+fi
 
 g++ -std=c++11 -Wall -Wextra -O2 main.cpp -o socialnet
 
